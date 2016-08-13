3fa053bc35b125c9238251c13ce4eadf0d21f47f
Milos Bejda <mbejda@live.com>
2016-08-12 22:38:27 -0400

    save

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sassApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sassApp
+ */
+angular.module('sassApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sassApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sassApp
+ */
+angular.module('sassApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
